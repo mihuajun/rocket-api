758e45b45fb17537b54027dc831a16c2b6551408
kobe96688@126.com <kobe96688@126.com>
2020-07-03 17:57:25 +0800

    1/添加跨域支持

--- src/main/java/com/github/alenfive/dataway2/config/Dataway2AutoConfig.java
+++ src/main/java/com/github/alenfive/dataway2/config/Dataway2AutoConfig.java
@@ -13,12 +13,34 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 @Configuration
 @EnableConfigurationProperties({Dataway2Properties.class})
 @ConditionalOnBean(DataSourceManager.class)
 public class Dataway2AutoConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 1允许任何域名使用
+        corsConfiguration.addAllowedOrigin("*");
+        // 2允许任何头
+        corsConfiguration.addAllowedHeader("*");
+        // 3允许任何方法（post、get等）
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
 
     @Bean
     @ConditionalOnMissingBean
