ea312ea0fac8dda0dcbd3f38ee1c2bec59e1419a
kobe96688@126.com <kobe96688@126.com>
2020-07-26 17:59:29 +0800

    1/会话信息信息

--- src/main/java/com/github/alenfive/rocketapi/utils/LoginUtils.java
+++ src/main/java/com/github/alenfive/rocketapi/utils/LoginUtils.java
@@ -11,11 +11,11 @@ public class LoginUtils {
     private final static String userIdent = "rocket-user";
 
     public static String getUser(HttpServletRequest request){
-        Object user = request.getHeader(userIdent);
+        Object user = request.getSession().getAttribute(userIdent);
         return user == null?null:user.toString();
     }
 
     public static void setUser(HttpServletRequest request, HttpServletResponse response, String user) {
-        response.setHeader(userIdent,user);
+        request.getSession().setAttribute(userIdent,user);
     }
 }
