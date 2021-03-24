2373c827ce8f00ce7832944ccb7d11bd02b6ee8e
mihuajun <mihuajun@wangxiaobao.com>
2021-03-24 20:23:21 +0800

    1. 切换数据源默认配置

--- src/main/java/com/github/alenfive/rocketapi/config/DefaultDataSourceManager.java
+++ src/main/java/com/github/alenfive/rocketapi/config/DefaultDataSourceManager.java
@@ -28,10 +28,10 @@ public class DefaultDataSourceManager extends DataSourceManager {
     @PostConstruct
     public void init() {
         Map<String, DataSourceDialect> dialects = new LinkedHashMap<>();
-        dialects.put("mysql",new MySQLDataSource(jdbcTemplate,false));
+        dialects.put("mysql",new MySQLDataSource(jdbcTemplate,true));
         //dialects.put("postgres",new PostgreSQLDataSource(jdbcTemplate,false));
         //dialects.put("oracle",new OracleDataSource(jdbcTemplate,true));
-        dialects.put("mongodb",new MongoDataSource(mongoTemplate,true));
+        dialects.put("mongodb",new MongoDataSource(mongoTemplate,false));
         super.setDialectMap(dialects);
     }
 }
