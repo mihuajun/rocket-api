628ad4515154e75c34a1c99a41445f31a6a9935a
mihuajun <mihuajun@wangxiaobao.com>
2021-07-27 14:13:51 +0800

    1.添加oracle驱动

--- src/main/java/com/github/alenfive/rocketapi/datasource/factory/OracleDriver.java
+++ src/main/java/com/github/alenfive/rocketapi/datasource/factory/OracleDriver.java
@@ -3,7 +3,9 @@ package com.github.alenfive.rocketapi.datasource.factory;
 import com.github.alenfive.rocketapi.datasource.DataSourceDialect;
 import com.github.alenfive.rocketapi.datasource.OracleDataSource;
 import com.github.alenfive.rocketapi.entity.DBConfig;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OracleDriver extends JdbcDriver {
     @Override
     String getName() {
